4ce11f6efdd2e39f734598377a32393bbaba5923
Masnoon Junaid <>
2020-12-26 20:33:42 +0530

    data displayed

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ const urlBase = "https://api.punkapi.com/v2/beers";
 
 
 async function getBeers() {
-	//fetch 
+	//fetch
 	const beerPromise = await fetch(urlBase);
-	const beer = await beerPromise.json();
+	const beers = await beerPromise.json();
 
 	const beersDiv = document.querySelector('.beers');
 
 	let beerHtml = "";
 
+
+
 	beers.forEach(beer => {
 		 beerHtml += `
-		 <h3>${beer.name}</h3>
+		 <div class='beer-wrapper card'>
+					 <div class='beer'>
+							 <img class='beer__img' src="${beer.image_url}">
+							 <h3>${beer.name}</h3>
+							 <span class='beer__info'>
+									 <span>ABV: ${beer.abv}%</span>
+									 <span>IBU: ${beer.ibu}</span>
+							 </span>
+					 </div>
+			 </div>
 		`;
 	});
-	beersDiv.innerHTML = beerHtml;
+
+
+	 beersDiv.innerHTML = beerHtml;
 }
+
 
 getBeers();
