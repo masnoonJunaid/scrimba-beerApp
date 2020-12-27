3b2139997dbbaf3abff215728bc27839250d3e55
Masnoon Junaid <>
2020-12-27 20:53:18 +0530

    pagination

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const urlBase = "https://api.punkapi.com/v2/beers";
+const urlBase = "https://api.punkapi.com/v2/beers?page=";
 const filterABV = document.getElementById("filterABV");
 const filterIBU = document.getElementById("filterIBU")
-let optionsABV = "", optionsIBU = "";
+const pageText = document.getElementById("pageNumber");
+const prevPage = document.getElementById("prevPage");
+const nextPage = document.getElementById("nextPage");
+let optionsABV = "", optionsIBU = "", page = 1;
+
+
 
 //filters
 filterABV.addEventListener("change", e => {
@@ -12,16 +17,16 @@ filterABV.addEventListener("change", e => {
 				optionsABV = "";
 				break
 		case "weak":
-				optionsABV = "abv_lt=4.6";
+				optionsABV = "&abv_lt=4.6";
 				break
 		case "medium":
-				optionsABV = "abv_gt=4.5&abv_lt=7.6";
+				optionsABV = "&abv_gt=4.5&abv_lt=7.6";
 				break
 		case "strong":
-				optionsABV = "abv_gt=7.5";
+				optionsABV = "&abv_gt=7.5";
 				break
 	}
-
+	page = 1;
 	getBeers();
 });
 
@@ -36,23 +41,43 @@ filterIBU.addEventListener("change", e => {
 			optionsIBU = "";
 			break;
 		case "weak":
-			optionsIBU = "ibu_lt=35";
+			optionsIBU = "&ibu_lt=35";
 			break
 		case "medium":
-			optionsIBU = "ibu_gt=34&ibu_lt=75";
+			optionsIBU = "&ibu_gt=34&ibu_lt=75";
 			break
 		case "strong":
-			optionsIBU = "ibu_gt=74";
+			optionsIBU = "&ibu_gt=74";
 			break
 	}
+	page = 1;
 	getBeers();
 })
 
 async function getBeers() {
-	const url = urlBase + "?" + optionsABV + "&" + optionsIBU;
+	const url = urlBase + page + optionsABV  + optionsIBU;
 	//fetch
-	const beerPromise = await fetch(urlBase);
+	const beerPromise = await fetch(url);
 	const beers = await beerPromise.json();
+
+	//pagination
+	pageText.innerText = page;
+
+	if(page ===  1){
+		prevPage.disabled = true;
+	} else {
+		prevPage.disabled = false
+	}
+
+	if(beers.length < 25) {
+		nextPage.disabled = true;
+	} else {
+		nextPage.disabled = false;
+	}
+
+
+
+
 
 	const beersDiv = document.querySelector('.beers');
 
@@ -83,9 +108,20 @@ async function getBeers() {
 		`;
 	});
 
-
 	 beersDiv.innerHTML = beerHtml;
 }
 
+//pagination
+prevPage.addEventListener('click', () => {
+	page--;
+	getBeers();
+});
 
+nextPage.addEventListener('click', () => {
+	page++;
+	getBeers();
+})
+
+
+//initial beer rendring
 getBeers();
